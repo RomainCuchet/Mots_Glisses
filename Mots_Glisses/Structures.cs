7945c42288daffe2974dd4919514ee90db984f1c
RomainCuchet <romain.cuchet02@gmail.com>
2023-12-10 16:55:59 +0100

    added sounds

---
+++ Mots_Glisses/Structures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mots_Glisses
+{
+    internal class Structures
+    {
+        public struct cmd
+        {
+            public Action function;
+            public string prompt;
+        }
+    }
+}
